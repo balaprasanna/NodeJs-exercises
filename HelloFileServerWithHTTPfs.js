24bf17935875567f7a69563c1930265ef17f803f
balaprasanna <balaprasannav2009@gmail.com>
2015-07-31 16:21:37 +0800

    File server with Http module

---
+++ HelloFileServerWithHTTPfs.js
@@ -0,0 +1,15 @@
+var http = require('http')
+var fs = require('fs')
+var port = process.argv[2]
+var path = process.argv[3]
+var server = http.createServer(function (req, res) {
+	res.writeHead(200, { 'content-type': 'text/plain' })
+	fs.createReadStream(path).pipe(res)
+	// fs.readFile(path, function (err, data) {
+	//   if (err) throw err;
+	//   //console.log(data);
+	//   res.write(data)
+	//   res.end()
+	// });
+})
+server.listen(port)
