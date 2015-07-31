deb1859b56add47a742b8e5505904c369b6f53fa
balaprasanna <balaprasannav2009@gmail.com>
2015-07-31 15:45:44 +0800

    Hello TCP

---
+++ HelloTcpSocketsWithNodenet.js
@@ -0,0 +1,13 @@
+var net = require('net')
+var strftime = require('strftime')
+var dateTitle = "YYYY-MM-DD hh:mm"
+var port = process.argv[2]
+var server = net.createServer(function (socket) {
+	socket.write(dateTitle+'\n');
+	
+	setInterval(function(){
+		socket.write('\n'+strftime('%Y %m %d %H:%M'))
+	},2000);
+	
+	})
+server.listen(port);
